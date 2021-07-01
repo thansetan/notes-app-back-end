6c4ecea22fe80d29890ebd9a2355c0a385749851
thansetan <62317096+thansetan@users.noreply.github.com>
2021-07-01 20:45:36 +0700

    tambah dikit lagi

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 
 const init = async () => {
     const server = hapi.server({
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         port: 5000,
         routes: {
             cors: {
